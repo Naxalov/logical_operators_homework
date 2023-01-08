f63cf2edfb948d696d4e4d9fd697d940819bbf23
ZARIFMAC <uzbzarif@gmail.com>
2023-01-08 16:13:23 +0500

    20

--- logic20.py
+++ logic20.py
@@ -8,5 +8,17 @@ def main(n):
     Returns:
         bool: answer
     """
+    x1 = n%10
+    n//=10
+    x2 = n%10
+    n//=10
+    x3=n%10
+    n//=10
+    x4=n%10
+    n//=10
+    x5=n%10
+    print(x1,x2,x3,x4,x5)
 
     return
+
+print(main(111))
