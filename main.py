4393091a1bfaedef905f5e4a7bc9055279eebd03
ZARIFMAC <uzbzarif@gmail.com>
2023-01-08 20:24:28 +0500

    change files

--- main.py
+++ main.py
@@ -5,9 +5,13 @@ parser = argparse.ArgumentParser()
 
 # Add an argument
 parser.add_argument("arg1", help="description of arg1")
+# Add another argument
+parser.add_argument("arg2", help="description of arg2")
 
 # Parse the command-line arguments
 args = parser.parse_args()
 
 # Access the value of the argument
-print(f"arg1: {args.arg1}")
+print(type(args.arg1))
+print(f"{args.arg1.split()}")
+print(f"arg2: {args.arg2}")
